7527466abc78b21b67bbfc36ae6bbe47f932653f
wzq <wzq@wzqdeiMac.local>
2023-11-22 19:22:36 +0800

    2023-11-22

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::fs;
 use regex::Regex;
+extern crate ipnet;
+use std::net::Ipv4Addr;
+use ipnet::Ipv4Subnets;
 
-fn iprange_to_ipcidr(iprange:String) -> String {
-    let seperator = Regex::new(r"([ ,.]+)").expect("Invalid regex");
-    String::from(format!("hello,{}!", iprange))
+
+fn iprange_to_ipcidr(ipranges:&String) {
+    let seperator = Regex::new(r"([^\d\.\-]+)").expect("Invalid regex");
+    let result = seperator.split(&ipranges);
+    let ;
+    for iprange in result {
+        println!("{}", iprange);
+    }
 }
 
 fn main() {
-    let file_path= "../iprange.txt";
+    let file_path= "./iprange.txt";
     let contents = fs::read_to_string(file_path).expect("open the file!");
-    println!("{}", contents);
-    // println!("{}", iprange_to_ipcidr(String::from("world")));
+    iprange_to_ipcidr(&contents)
 }
