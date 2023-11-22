b0199be390ab21bd61677ac076eaf2efeabe274d
wzq <347596212@qq.com>
2023-11-22 20:31:26 +0800

    同步

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ use ipnet::Ipv4Subnets;
 fn iprange_to_ipcidr(ipranges:&String) {
     let seperator = Regex::new(r"([^\d\.\-]+)").expect("Invalid regex");
     let result = seperator.split(&ipranges);
-    let ;
+    let iprange_list: Vec<Vec<String>> = Vec::new();
     for iprange in result {
-        println!("{}", iprange);
+        let iprange_start: String= [iprange.split(&['.','-']).take(4).collect::Vec<&String>()].join(",");
+ //       let a = iprange_start.join(",");
+        println!("{:?}", iprange_start);
     }
 }
 
