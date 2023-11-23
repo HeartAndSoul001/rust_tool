5696b447c1a06f55553a9f48157ee975c7677d93
wzq <wzq@MacBook-Pro.local>
2023-11-23 21:39:02 +0800

    同步

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, clone};
+use std::fs;
 use regex::Regex;
 extern crate ipnet;
 use std::net::Ipv4Addr;
@@ -8,16 +8,14 @@ use ipnet::Ipv4Subnets;
 fn iprange_to_ipcidr(ipranges:&String) {
     let seperator = Regex::new(r"([^\d\.\-]+)").expect("Invalid regex");
     let result = seperator.split(&ipranges);
-    let iprange_list: Vec<Vec<String>> = Vec::new();
+    //let iprange_list: Vec<Vec<String>> = Vec::new();
     for iprange in result {
         let ip_vec: Vec<&str> = iprange.split(&['.','-']).collect();
-        let (iprange_0_3, iprange_head,iprange_tail) = (ip_vec[0..3].to_vec(), ip_vec[3], ip_vec.last().unwrap());
-        //let (mut iprange_start_list, mut iprange_end_list) = (iprange_0_3.clone(), iprange_0_3.clone());
-        //iprange_start_list.push(iprange_head);
-        //iprange_end_list.push(iprange_tail);
-        //let (iprange_start, iprange_end) = (iprange_start_list.join("."), iprange_end_list.join("."));
-        //println!("{}",iprange_start);
-        //println!("{}",iprange_end);
+        let iprange_0_3 = ip_vec[0..3].to_vec().join(".");
+        let iprange_head = format!("{iprange_0_3}.{}", ip_vec[3]);
+        let iprange_tail = format!("{iprange_0_3}.{}", *ip_vec.last().unwrap());
+        println!("{}", iprange_head);
+        println!("{}", iprange_tail);
     }
 }
 
