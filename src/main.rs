4f2315eda96fe3114316956730aa75facee17396
wzq <wzq@MacBook-Pro.local>
2023-11-23 14:04:29 +0800

    同步

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, clone};
 use regex::Regex;
 extern crate ipnet;
 use std::net::Ipv4Addr;
@@ -10,9 +10,14 @@ fn iprange_to_ipcidr(ipranges:&String) {
     let result = seperator.split(&ipranges);
     let iprange_list: Vec<Vec<String>> = Vec::new();
     for iprange in result {
-        let iprange_start: String= [iprange.split(&['.','-']).take(4).collect::Vec<&String>()].join(",");
- //       let a = iprange_start.join(",");
-        println!("{:?}", iprange_start);
+        let ip_vec: Vec<&str> = iprange.split(&['.','-']).collect();
+        let (iprange_0_3, iprange_head,iprange_tail) = (ip_vec[0..3].to_vec(), ip_vec[3], ip_vec.last().unwrap());
+        //let (mut iprange_start_list, mut iprange_end_list) = (iprange_0_3.clone(), iprange_0_3.clone());
+        //iprange_start_list.push(iprange_head);
+        //iprange_end_list.push(iprange_tail);
+        //let (iprange_start, iprange_end) = (iprange_start_list.join("."), iprange_end_list.join("."));
+        //println!("{}",iprange_start);
+        //println!("{}",iprange_end);
     }
 }
 
